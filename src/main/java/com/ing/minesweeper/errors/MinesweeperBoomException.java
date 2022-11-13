d2688e0bff4a6ae507ef654df7621fa2aade5841
Tomasz Wolinski <tomasz.wolinski@yolt.eu>
2022-11-13 17:40:56 +0100

    Implementation

--- src/main/java/com/ing/minesweeper/errors/MinesweeperBoomException.java
+++ src/main/java/com/ing/minesweeper/errors/MinesweeperBoomException.java
@@ -1,6 +1,7 @@
 package com.ing.minesweeper.errors;
 
 public class MinesweeperBoomException extends MinesweeperException {
+
     public MinesweeperBoomException(String message) {
         super(message);
     }
